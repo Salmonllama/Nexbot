c6f77a477928c173f3418836e106b25931f541e4
Crablet <a.gryczewski999@gmail.com>
2018-11-24 02:01:32 -0500

    registering and collecting commands

---
+++ src/main/java/org/crabcraft/nexbot/commandler/CommandRegistry.java
@@ -0,0 +1,20 @@
+package org.crabcraft.nexbot.commandler;
+
+import java.util.TreeMap;
+
+public class CommandRegistry {
+    private static TreeMap<String, Command> commands;
+
+    public CommandRegistry() {
+        commands = new TreeMap<>();
+    }
+
+    public static Command registerCommand(Command command) {
+        commands.put(command.Aliases().get(0), command);
+        return command;
+    }
+
+    public TreeMap<String, Command> getCommands() {
+        return commands;
+    }
+}
