3fc01471c647f2a018ce2f9d6a47f43ca2357c55
Crablet <a.gryczewski999@gmail.com>
2018-11-24 02:38:47 -0500

    Command for testing

---
+++ src/main/java/org/crabcraft/nexbot/commands/TestCommand.java
@@ -0,0 +1,26 @@
+package org.crabcraft.nexbot.commands;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.crabcraft.nexbot.commandler.Command;
+import org.javacord.api.event.message.MessageCreateEvent;
+
+public class TestCommand extends Command {
+
+    @Override
+    public String Name() { return "Test Command"; }
+    @Override
+    public List<String> Aliases() { return Arrays.asList("test"); }
+    @Override
+    public String Description() { return "Just a test command, look the other way"; }
+    @Override
+    public String Usage() { return "test"; }
+    @Override
+    public List<String> Permissions() { return Arrays.asList("none"); }
+
+    @Override
+    public void onCommand(MessageCreateEvent event, String[] args) {
+        sendResponse(event, "test");
+    }
+}
