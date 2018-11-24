929fbdd8adb48baff016e969e02cca82868da07e
Crablet <a.gryczewski999@gmail.com>
2018-11-23 19:59:44 -0500

    rearranged methods' logic. Added serverSetup method.

--- src/main/java/org/crabcraft/nexbot/utilities/Database.java
+++ src/main/java/org/crabcraft/nexbot/utilities/Database.java
@@ -7,7 +7,8 @@ import java.sql.SQLException;
 
 public class Database {
 
-    public static void firstTimeSetup() {
+    public static Connection connectionSetup() {
+        // Establish the database connection and return the connection variable
         try {
             Class.forName("org.sqlite.JDBC");
         } 
@@ -15,20 +16,32 @@ public class Database {
         {
             e.printStackTrace();
         }
-    
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:nexbot.db");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public static void firstTimeSetup() {
         // Check if nexbot.db exists, if not, create it.
         if (new File("nexbot.db").exists()) {
             System.out.println("Database exists, no need for further action.");
         }
         else {
-            try {
-                Connection conn = DriverManager.getConnection("jdbc:sqlite:nexbot.db");
-                conn.close();
-                System.out.println("database created");
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
+            Connection conn = connectionSetup();
+            try{ conn.close(); } catch(SQLException e) { e.printStackTrace(); }
+            System.out.println("Database did not exist and was created");
         }
+    }
+
+    public static void serverFirstTimeSetup() {
+        // When a server is joined, this is called to add it to the database table.
+        // TODO: serverConf table with PREFIX, LOGCHANNEL.
     }
 }
