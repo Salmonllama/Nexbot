115f66023c573a7fdb54e989788bf5669aa71611
Crablet <a.gryczewski999@gmail.com>
2018-11-29 20:58:06 -0500

    update for noPerms

--- src/main/java/org/crabcraft/nexbot/utilities/PrefabResponses.java
+++ src/main/java/org/crabcraft/nexbot/utilities/PrefabResponses.java
@@ -1,4 +1,19 @@
 package org.crabcraft.nexbot.utilities;
 
+import java.awt.Color;
+
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.event.message.MessageCreateEvent;
+
 public class PrefabResponses {
+
+    public static EmbedBuilder noPermissions(MessageCreateEvent event, String permission) {
+
+        EmbedBuilder embed = new EmbedBuilder()
+            .setColor(Color.RED)
+            .setAuthor(event.getApi().getYourself())
+            .addField("Missing Permissions:", permission);
+
+        return embed;
+    }
 }
