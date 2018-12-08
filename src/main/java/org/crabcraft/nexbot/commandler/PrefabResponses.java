479aee8047f88d81d2f95fb8e52972d3522c9d3c
Crablet <a.gryczewski999@gmail.com>
2018-12-08 11:11:21 -0500

    Made prefabs package-private

--- src/main/java/org/crabcraft/nexbot/commandler/PrefabResponses.java
+++ src/main/java/org/crabcraft/nexbot/commandler/PrefabResponses.java
@@ -5,9 +5,9 @@ import java.awt.Color;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
-public class PrefabResponses {
+class PrefabResponses {
 
-    public static EmbedBuilder noPermissions(MessageCreateEvent event, String permission) {
+    static EmbedBuilder noPermissions(MessageCreateEvent event, String permission) {
 
         EmbedBuilder embed = new EmbedBuilder()
             .setColor(Color.RED)
